630569ee6a2ba045857025e0aa666d7ec48e1bb0
Aibar <akenbay@icloud.com>
2025-07-29 21:07:30 +0500

    Fix: fixed comment structure and services

--- internal/domain/comment.go
+++ internal/domain/comment.go
@@ -2,24 +2,24 @@ package domain
 
 import (
 	"context"
+	"mime/multipart"
 	"time"
 )
 
 type Comment struct {
-	ID         string  // UUID
-	PostID     string  // Parent post UUID
-	ParentID   *string // Nullable (for nested comments)
-	User       User    // Embedded or reference SessionID
-	Content    string
-	ImageKey   *string // S3 object key (nullable)
-	BucketName *string // S3 bucket (nullable)
-	CreatedAt  time.Time
+	ID        string  // UUID
+	PostID    string  // Parent post UUID
+	ParentID  *string // Nullable (for nested comments)
+	User      User    // Embedded or reference SessionID
+	Content   string
+	ImageURLs []string
+	CreatedAt time.Time
 }
 
 type CreateCommentReq struct {
 	SessionID string
 	Content   string
-	ImageData []byte
+	ImageData []*multipart.FileHeader
 }
 
 type CommentRepository interface {
--- internal/services/commentService.go
+++ internal/services/commentService.go
@@ -7,34 +7,50 @@ import (
 )
 
 type CommentService struct {
-	commentRepo    domain.CommentRepository
-	userService    UserService
-	imageStorage   domain.ImageStorageAPI
-	imageValidator domain.ImageValidator
-	defaultBucket  string
+	commentRepo   domain.CommentRepository
+	userService   UserService
+	imageStorage  domain.ImageStorageAPI
+	fileUtils     domain.FileUtils
+	defaultBucket string
+}
+
+func NewCommentService(commentRepo domain.CommentRepository, userService UserService, imageStorage domain.ImageStorageAPI, fileUtils domain.FileUtils, defaultBucket string) *CommentService {
+	return &CommentService{
+		commentRepo:   commentRepo,
+		userService:   userService,
+		imageStorage:  imageStorage,
+		fileUtils:     fileUtils,
+		defaultBucket: defaultBucket,
+	}
 }
 
 func (s *CommentService) CreateComment(ctx context.Context, createCommentReq *domain.CreateCommentReq) (string, error) {
 	var comment domain.Comment
 
-	if err := s.imageValidator.Validate(createCommentReq.ImageData); err != nil {
-		slog.Error("Failed to validate the image", "error", err)
-		return "", err
-	}
+	for _, fileheader := range createCommentReq.ImageData {
 
-	if createCommentReq.ImageData != nil {
-		image_key, err := s.imageStorage.Store(createCommentReq.ImageData, s.defaultBucket)
+		// Validate if its image
+		if err := s.fileUtils.ValidateImage(fileheader); err != nil {
+			slog.Error("Failed to validate the image", "error", err)
+			return "", err
+		}
+
+		// Convert into bytes
+		fileBytes, err := s.fileUtils.FileHeaderToBytes(fileheader)
+		if err != nil {
+			slog.Error("Failed to convert image into bytes.")
+			return "", err
+		}
+
+		imageURL, err := s.imageStorage.Store(fileBytes, s.defaultBucket)
 		if err != nil {
 			slog.Error("Failed to store the image", "error", err)
 			return "", err
 		}
-		comment.ImageKey = &image_key
-	} else {
-		comment.ImageKey = nil
+		comment.ImageURLs = append(comment.ImageURLs, imageURL)
 	}
 
 	comment.Content = createCommentReq.Content
-	comment.BucketName = &s.defaultBucket
 	sessionID := createCommentReq.SessionID
 
 	user, err := s.userService.FindUserByID(ctx, sessionID)
